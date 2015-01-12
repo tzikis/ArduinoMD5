809513ea5c247c1099dbe5a45949fce23466816b
spaniakos <spaniakos@gmail.com>
2015-01-12 15:54:15 +0100

    RPi fixes

--- MD5_config.h
+++ MD5_config.h
@@ -9,6 +9,8 @@
   #include <stdio.h>
   #include <stdlib.h>
   #include <string.h>
+  #include <sys/time.h>
+  #include <unistd.h>
 #else
   #include <Arduino.h>
 #endif
--- examples_Rpi/MD5_Hash.cpp
+++ examples_Rpi/MD5_Hash.cpp
@@ -1,14 +1,16 @@
 #include <MD5.h>
 
+MD5 md5;
+
 int main(int argc, char** argv) 
 {
   double ms;
   //generate the MD5 hash for our string
-  ms = MD5::millis();
-  unsigned char* hash=MD5::make_hash("abc");
+  ms = md5.millis();
+  unsigned char* hash=md5.make_hash("abc");
   //generate the digest (hex encoding) of our hash
-  char *md5str = MD5::make_digest(hash, 16);
-  printf("Done in (%f) ms",(MD5::millis() - ms));
+  char *md5str = md5.make_digest(hash, 16);
+  printf("Done in (%f) ms\n",(md5.millis() - ms));
   free(hash);
   //print it on our serial monitor
   printf("PLAIN   :abc\n");
@@ -18,11 +20,11 @@ int main(int argc, char** argv)
   printf("\n");
   
   //generate the MD5 hash for our string
-  ms = MD5::millis();
-  hash=MD5::make_hash("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+  ms = md5.millis();
+  hash= md5.make_hash("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
   //generate the digest (hex encoding) of our hash
-  md5str = MD5::make_digest(hash, 16);
-  printf("Done in (%f) ms",(MD5::millis() - ms));
+  md5str = md5.make_digest(hash, 16);
+  printf("Done in (%f) ms\n",(md5.millis() - ms));
   printf("PLAIN   :abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq\n");
   printf("EXPECTED:8215ef0796a20bcaaae116d3876c664a\n");
   printf("RESULT  :");
@@ -30,15 +32,15 @@ int main(int argc, char** argv)
   printf("\n");
   
   //generate the MD5 hash for our string
-  char aa[100001];
+  char aa[1000001];
   for (int i=0;i<1000000;i++)
     aa[i] = 'a';
   aa[1000000] = '\0';
-  ms = MD5::millis();
-  hash=MD5::make_hash(aa);
+  ms = md5.millis();
+  hash= md5.make_hash(aa);
   //generate the digest (hex encoding) of our hash
-  md5str = MD5::make_digest(hash, 16);
-  printf("Done in (%f) ms",(MD5::millis() - ms));
+  md5str = md5.make_digest(hash, 16);
+  printf("Done in (%f) ms\n",(md5.millis() - ms));
   printf("PLAIN   :1,000,000 x a\n");
   printf("EXPECTED:7707d6ae4e027c70eea2a935c2296f21\n");
   printf("RESULT  :");
