8e1c6a67b14edc6d5b3854cf3a0f9552bc4a9071
Georgios Spanos <spaniakos@gmail.com>
2015-01-18 21:23:40 +0100

    Update MD5_config.h

--- MD5_config.h
+++ MD5_config.h
@@ -1,6 +1,10 @@
 #ifndef MD5_config_h
 #define MD5_config_h
 
+#define HMAC_IPAD 0x36
+#define HMAC_OPAD 0x5c
+#define BLOCK_SIZE 16
+
 #if  (defined(__linux) || defined(linux)) && !defined(__ARDUINO_X86__)
 
   #define MD5_LINUX
